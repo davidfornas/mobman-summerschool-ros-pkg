019a619203f1e43cdfdc376f39eaa01dea6eab22
David Fornas <davidfornas@gmail.com>
2015-06-30 13:23:26 +0200

    Add ROS param support

--- vehicle_perception/src/surf_detection_example.cpp
+++ vehicle_perception/src/surf_detection_example.cpp
@@ -3,9 +3,20 @@
 int main(int argc, char** argv)
 {
 
-  ros::init(argc, argv, "surf_detection_" + std::string(argv[1]));
-  SurfDetector sd("/phone1/camera/image", argv[1], argv[1] + std::string("error"), argv[2]);
-  sd.setDisplay(false);
+  ros::init(argc, argv, "surf_detection");
+  ros::NodeHandle nh;
+
+  std::string image_topic("/phone1/camera/image"), debug_image("/debug"), error_topic("/error"), template_file_path("clip.jpg");
+  bool display_image;
+
+  nh.getParam("image_topic", image_topic);
+  nh.getParam("debug_image", debug_image);
+  nh.getParam("error_topic", error_topic);
+  nh.getParam("template_file_path", template_file_path);
+  nh.param("display_image", display_image, false);
+
+  SurfDetector sd(image_topic, debug_image, error_topic, template_file_path);
+  sd.setDisplay(display_image);
   ros::spin();
   return 0;
 
