9bb16fddb96101b4ccabf375b5855c2a1784490d
David Fornas <davidfornas@gmail.com>
2015-07-07 13:54:41 +0200

    Improve Servo control and comms.

--- arduino/VehicleControl/VehicleControl.ino
+++ arduino/VehicleControl/VehicleControl.ino
@@ -2,19 +2,14 @@
 #include <SoftwareSerial.h>
  
 SoftwareSerial mySerial(6, 7); // RX, TX
-String command = "", param=""; // Stores response of bluetooth device
-	                     // which simply allows \n between each
-	                     // response.
 
-void issueCommand(String command, String param){
+void issueCommand(String command, String param1, String param2){
   if(command=="SERVO"){
-    moveSonar(param);
-  }else if(command=="RIGHT"){
-    moveRight(param);
-  }else if(command=="LEFT"){
-    moveLeft(param);
+    moveSonar(param1);
+  }else if(command=="WHEELS"){
+    moveWheels(param1, param2);
   }else if(command=="SONAR"){
-    sense(param);
+    sense();
   }else{   
     mySerial.println("BAD COMMAND");
   }        
@@ -38,33 +33,28 @@ volatile int ok = LOW;  // Valid measure
 volatile unsigned long now = 0;
 volatile unsigned long before = 0;
 
-void moveLeft(String spd){
-  //Reading parameters
+void moveWheels(String lspd, String rspd){
+
   Serial.print("Moving left wheel at speed: ");
-  Serial.println(spd.toInt());
-  left_servo.write(spd.toInt());   
+  Serial.println(lspd.toInt());
+  left_servo.write(lspd.toInt());  
+  
+  Serial.print("Moving right wheel at speed: ");
+  Serial.println(rspd.toInt());
+  right_servo.write(rspd.toInt());  
   mySerial.println("MOVED");
   servo_counter=0;
 }   
 
-void moveRight(String spd){
-  //Reading parameters
-  Serial.print("Moving right wheel at speed: ");
-  Serial.println(spd.toInt());
-  right_servo.write(spd.toInt());   
-  mySerial.println("MOVED");
-  servo_counter=0;                  
-}  
-
-void moveSonar(String spd){
+ void moveSonar(String spd){   
   //Reading parameters
   Serial.print("Moving sonar to position: ");
   Serial.println(spd.toInt());
-  sonar_servo.write(spd.toInt());   
+  sonar_servo.write(spd.toInt());  
   mySerial.println("MOVED");                
 }  
         
-void sense(String param){
+void sense(){
   attachInterrupt(1, measure, CHANGE);   // Activa interrupción en pin de entrada (3)  
   digitalWrite(MEASURE, HIGH);          // Activa medidor de distancia
   delay(10);                          // Para dar tiempo a realizar la primera medición
@@ -96,15 +86,14 @@ void sense(String param){
 
 void setup()
 {
-  // Asigna pines a los servos
+    // Asigna pines a los servos
   right_servo.attach(9);        // Asigna el servo derecho al pin 9 (OUTPUT 2 de la TINKERKIT)
   left_servo.attach(10);       // Asigna el servo izquierdo al pin 10 (OUTPUT 1 de la TINKERKIT)
   sonar_servo.attach(11);    // Asigna el miniservo al pin 11 (OUTPUT 0 de la TINKERKIT)
-
-  // Inmoviliza servos  
-  right_servo.write(90);  
-  left_servo.write(90);  
-  sonar_servo.write(90);    
+    
+  //  Init position
+  moveWheels("90", "90");
+  moveSonar("90");    
   
   pinMode(INTERRUPT_INPUT, INPUT);           // Pin de entrada del medidor
   pinMode(MEASURE, OUTPUT);            // Señal de disparo del medidor
@@ -120,35 +109,38 @@ void setup()
  
 void loop()
 {
- 
+     String command = "", param1="", param2=""; // Stores response of bluetooth device
+	                     // which simply allows \n between each
+	                     // response.
 	  // Polling. Read BT input if available.
 	  if (mySerial.available()) {
-            boolean readingParam=false;
+            boolean readingParam1=false, readingParam2=false;
 	    while(mySerial.available()) { // While there is more to be read, keep reading.
               char r = (char)mySerial.read();
-	      if(r!='+'&&!readingParam){
+	      if( r != '+' && !readingParam1 && !readingParam2){
                 command += r; 
               }
-              if(readingParam){
-               param += r; 
+              if( r != '+' && readingParam1 && !readingParam2){
+               param1 += r; 
+              }
+              if( r != '+' && readingParam2){
+               param2 += r; 
               }
               if(r=='+'){
-               readingParam=true; 
+               if(readingParam1) readingParam2=true;
+               readingParam1=true; 
               }
 	    }
 	    Serial.print("Received command: ");
 	    Serial.println(command);
-	    Serial.print("With parameter: ");
-	    Serial.println(param);
-            issueCommand(command, param);
-            command = ""; // No repeats
-            param="";
+	    Serial.print("With parameter 1: ");
+	    Serial.println(param1);
+	    Serial.print("With parameter 2: ");
+	    Serial.println(param2);
+            issueCommand(command, param1, param2);
+
 	  }
-  delay(50);                   // Insertamos una espera
-  if(servo_counter==25){
-   left_servo.write(90); 
-   right_servo.write(90);
- }  servo_counter +=1;
+  delay(20);                   // Insertamos una espera
  
 }
 
