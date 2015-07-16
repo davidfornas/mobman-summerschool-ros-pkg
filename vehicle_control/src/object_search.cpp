b9fcf3736c61074119f5883c97581b591cf465c5
David Fornas <davidfornas@gmail.com>
2015-07-16 13:17:49 +0200

    Improve example

--- vehicle_control/src/object_search.cpp
+++ vehicle_control/src/object_search.cpp
@@ -8,18 +8,19 @@ void errorCallback(const std_msgs::Int32::ConstPtr& msg){
   error_x = msg->data;
 }
 
-
-
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "object_search");
   ros::NodeHandle nh;
 
   ros::Publisher wheels_pub;
+  ros::Publisher gripper_pub;
+
   ros::Subscriber error_x_sub;
 
-  //Publish sensors
   wheels_pub = nh.advertise<std_msgs::Int32MultiArray>("/wheels_cmd", 1);
+  gripper_pub = nh.advertise<std_msgs::Int32>("/gripper", 1);
+
   error_x_sub = nh.subscribe("/error_x", 1, errorCallback);
 
   ros::Rate r(1);
@@ -27,8 +28,11 @@ int main(int argc, char *argv[])
   while(ros::ok()){
 
     std_msgs::Int32MultiArray control;
+    std_msgs::Int32 controlGripper;
+
     control.data.push_back(90);
     control.data.push_back(90);
+
 
     if(error_x > 600){
     control.data[0] = 80;
@@ -41,9 +45,22 @@ int main(int argc, char *argv[])
     }
 
     ros::Duration(0.5).sleep();
+
     control.data[0] = 90;
     control.data[1] = 90;
     wheels_pub.publish(control);
+
+    //Alternatively, use Gripper to notify user
+
+    /*
+    if(error_x > 100){
+        controlGripper.data = 1;
+        gripper_pub.publish(controlGripper);
+    }else if(error_x < 100){
+      controlGripper.data = 0;
+      gripper_pub.publish(controlGripper);
+    }
+    */
 
     r.sleep();
     ros::spinOnce();
